eadeed4f1e37ad48901f7e5c1291d26c582573aa
Renyambar <ambarwatireny73@gmail.com>
2025-09-23 00:31:18 +0700

    Jobsheet4_soal2

---
+++ tipe_data.php
@@ -0,0 +1,36 @@
+<?php
+$a = 10;
+$b = 5;
+$c = $a+5;
+$d = $b + (10 *5);
+$e = $d - $c;
+
+echo "Variabel a: {$a} <br>";
+echo "Variabel b: {$b} <br>";
+echo "Variabel c: {$c} <br>";
+echo "Variabel d: {$d} <br>";
+echo "Variabel e: {$e} <br>";
+
+var_dump($e);
+
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
+
+$namaDepan = "Ibnu";
+$namaBelakang = "Jakaria";
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
+
+echo "Nama Depan: {$namaDepan} <br>";
+echo "Nama Belakang: " . $namaBelakang . "<br>";
+
+echo $namaLengkap;
+
+$listahasiswa = ["Wahid Abdullah", "Elmo Bachtiar", "Lenids Fabri"];
+echo $listahasiswa[0];
+?>
