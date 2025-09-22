dcb0c59fac62fc9234be4a87a8af3ce500055e32
Renyambar <ambarwatireny73@gmail.com>
2025-09-23 00:53:13 +0700

    Jobsheet4_soal3.2-3.3

--- operator.php
+++ operator.php
@@ -16,5 +16,29 @@ echo "Hasil Bagi: $hasilBagi <br>";
 echo "Sisa Bagi: $sisaBagi <br>";
 echo "Hasil Pangkat: $pangkat <br>";
 
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil Sama: $hasilSama <br>";
+echo "Hasil Tidak Sama: $hasilTidakSama <br>";
+echo "Hasil Lebih Kecil: $hasilLebihKecil <br>";
+echo "Hasil Lebih Besar: $hasilLebihBesar <br>";
+echo "Hasil Lebih Kecil Sama: $hasilLebihKecilSama <br>";
+echo "Hasil Lebih Besar Sama: $hasilLebihBesarSama <br>";
+
+$hasilAnd = $a && $b;
+$hasilOr = $a || $b;
+$hasilNotA = !$a;
+$hasilNotB = !$b;
+
+echo "Hasil And: $hasilAnd <br>";
+echo "Hasil Or: $hasilOr <br>";
+echo "Hasil Not A: $hasilNotA <br>";
+echo "Hasil Not B: $hasilNotB <br>";
+
 
 ?>
