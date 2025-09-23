f58cdb0dca0a31659735be391bb31b030467f2aa
Renyambar <ambarwatireny73@gmail.com>
2025-09-23 18:04:59 +0700

    Jobsheet4_soal4.6

--- struktur_kontrol.php
+++ struktur_kontrol.php
@@ -49,4 +49,35 @@ foreach ($nilaiSiswa as $nilai) {
     }
     echo "Nilai: $nilai (Lulus) <br>";
 }
+
+$nilai = [85, 92, 78, 64, 90, 75, 88, 79, 70, 96];
+// Urutkan nilai dari kecil ke besar
+$n = count($nilai);
+
+for ($i = 0; $i < $n-1; $i++) {
+    for ($j = 0; $j < $n-$i-1; $j++) {
+        if ($nilai[$j] > $nilai[$j+1]) {
+            $temp = $nilai[$j];
+            $nilai[$j] = $nilai[$j+1];
+            $nilai[$j+1] = $temp;
+        }
+    }
+}
+
+// Hapus 2 nilai terendah
+array_shift($nilai); 
+array_shift($nilai); 
+
+// Hapus 2 nilai tertinggi
+array_pop($nilai); 
+array_pop($nilai); 
+
+// Hitung total nilai yang tersisa
+$total = array_sum($nilai);
+
+echo "Total nilai setelah mengabaikan 2 tertinggi & 2 terendah: $total <br>";
+
+// rata-rata
+$rata_rata = $total / count($nilai);
+echo "Rata-rata: $rata_rata <br>";
 ?>
