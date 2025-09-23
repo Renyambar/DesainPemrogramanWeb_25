38288211f5d768473e7455ccd7ca2cd944fb6ac9
Renyambar <ambarwatireny73@gmail.com>
2025-09-23 07:29:28 +0700

    Jobsheet4_spal3.6

--- operator.php
+++ operator.php
@@ -57,4 +57,15 @@ echo "Hasil Identik: $hasilIdentik <br>";
 $hasilTidakIdentik = $a !== $b;
 echo "Hasil Tidak Identik: $hasilTidakIdentik <br>"; 
 
+$total_kursi = 45;
+$kursi_terisi = 28;
+// Hitung kursi kosong
+$kursi_kosong = $total_kursi - $kursi_terisi;
+// Hitung persentase kursi kosong
+$persentase_kosong = ($kursi_kosong / $total_kursi) * 100;
+
+echo "Total kursi: $total_kursi <br>";
+echo "Kursi terisi: $kursi_terisi <br>";
+echo "Kursi kosong: $kursi_kosong <br>";
+echo "Persentase kursi kosong: " . number_format($persentase_kosong, 2) . "%";
 ?>
