e1000ae679b8a29f64d1d7fc713df06f1886ee3e
Renyambar <ambarwatireny73@gmail.com>
2025-09-23 07:44:47 +0700

    Jobsheet4_soal4.3

--- struktur_kontrol.php
+++ struktur_kontrol.php
@@ -20,5 +20,15 @@ while ($jarakSaatIni < $jarakTarget) {
     $jarakSaatIni += $peningkatanHarian;
     $hari++;
 }
-echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.<br>";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i=1; $i <= $jumlahLahan ; $i++) { 
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah"
 ?>
