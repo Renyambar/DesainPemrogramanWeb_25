e21719bbf72a89fc5d975dbcbce2aaaea39bbbb6
Renyambar <ambarwatireny73@gmail.com>
2025-10-01 01:40:14 +0700

    Jobsheet5_soal5

--- JS05_PHP-2/function.php
+++ JS05_PHP-2/function.php
@@ -1,10 +1,15 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Reny Ambarwati<br/>";
+function perkenalan($nama, $salam){
+    echo $salam. ", ";
+    echo "Perkenalkan, nama saya ".$nama. "<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
-perkenalan();
-perkenalan();
+perkenalan("Hamdana", "Hallo");
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya, $ucapanSalam);
 ?>
